20c5352afbc7c0fd1818a5bb70ca0055f8044f7d
Jelle Akkerman <jelle@jelleakkerman.nl>
2012-09-04 12:09:07 +0200

    New docs.js format

--- www/docs.js
+++ www/docs.js
@@ -1,11 +1,28 @@
 // This file is for..
-var docs = [
+var docs = 
   {
-    file:"Documents/Graduation plan.markdown",
-    title: "Graduation Plan"
-  },
-  {
-    file:"Documents/Research plan.markdown",
-    title: "Research Plan"
+    ThesisPlan: {
+      dir: "Documents/GraduationPlan",
+      files: [
+        "Competency overview.markdown",
+        "Graduation plan.markdown",
+        "Motivation & Goals.markdown",
+        "Planning thesis.markdown",
+        "Questions & Goals.markdown",
+        "Summary thesis.markdown"
+      ],
+      title: "Graduation Plan"
+    },
+    ResearchPlan: {
+      dir: "Documents/ResearchPlan",
+      files: [
+        "Research plan.markdown"
+      ],
+      title: "Research Plan"
+    }
+    Thesis: {
+      dir: "Documents/Thesis",
+      files: [""],
+      title: "Thesis"
+    }
   }
-]
